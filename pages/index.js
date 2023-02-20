cf426b399951564eef533049d5549a873058705f
Ankit <abinashdwivedi69@gmail.com>
2023-02-20 16:02:13 +0530

    Fixed the padding problem for small devices

--- pages/index.js
+++ pages/index.js
@@ -7,25 +7,27 @@ import { FeaturedPosts } from '../sections';
 
 export default function Home({ posts }) {
   return (
-    <div className="container mx-auto px-10 sm:px-5 mb-8 ">
+    <div className="container mx-auto px-3 lg:px-10 mb-8 ">
       <Head>
         <title>OutWorld : Just Some Articles</title>
         <link rel="icon" href="/favicon.png" />
       </Head>
       <FeaturedPosts />
-      <div className='grid grid-cols-1 lg:grid-cols-12 gap-12'>
-        <div className='lg:col-span-8 col-span-1'>
-        {posts.map((post) => ( <PostCard post={post.node} key={post.title} /> ))}
+      <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
+        <div className="lg:col-span-8 col-span-1">
+          {posts.map((post) => (
+            <PostCard post={post.node} key={post.title} />
+          ))}
         </div>
-      <div className='lg:col-span-4 col-span-1'>
-          <div className='lg:sticky relative top-8'>
+        <div className="lg:col-span-4 col-span-1">
+          <div className="lg:sticky relative top-8">
             <PostWidget />
             <Categories />
           </div>
-      </div>
+        </div>
       </div>
     </div>
-  )
+  );
 }
 
 export async function getStaticProps() {
